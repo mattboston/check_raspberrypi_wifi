275d80b54ac6c91a24a53696818a2aa40b2dea31
Matt Shields <mattboston@users.noreply.github.com>
2017-02-24 14:52:29 -0500

    Update check_raspberrypi_wifi.sh

--- check_raspberrypi_wifi.sh
+++ check_raspberrypi_wifi.sh
@@ -4,5 +4,6 @@ if ifconfig wlan0 | grep -q "inet addr:" ; then
   echo "Connection up"
 else
   echo "Network connection down! Attempting reconnection."
- ifup --force wlan0
+  ifdown --force wlan0
+  ifup --force wlan0
 fi
