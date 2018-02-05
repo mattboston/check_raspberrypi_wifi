5744c2d8c3a1d4e86955e117d06b5d8aa48e0ec3
Matt Shields <mattboston@users.noreply.github.com>
2018-02-05 12:01:46 -0500

    Update check_raspberrypi_wifi.sh

--- check_raspberrypi_wifi.sh
+++ check_raspberrypi_wifi.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-if ifconfig wlan0 | grep -q "inet addr:" ; then
+INTERFACE="wlan0"
+GATEWAY=`route -n | grep 'UG[ \t]' | awk '{print $2}'`
+OCTET=`echo ${GATEWAY} | cut -d"." -f1`
+
+if ! [ -x "$(command -v fping)" ]; then
+  echo "Error: fping not installed" >&2
+  exit 1
+fi
+
+ifconfig ${INTERFACE} | egrep -q "inet(.*)${OCTET}"
+RESULT1=`echo $?`
+fping ${GATEWAY} | grep -q "alive"
+RESULT2=`echo $?`
+
+if [ ${RESULT1} -eq 0 ] && [ ${RESULT2} -eq 0 ]
+then
   echo "Connection up"
 else
   echo "Network connection down! Attempting reconnection."
-  ifdown --force wlan0
-  ifup --force wlan0
+  ifdown --force ${INTERFACE}
+  ifup --force ${INTERFACE}
 fi
